e837a5a7399ba9793facc39b35dd558f31eb52ce
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-09-12 15:02:27 -0300

    Eureka Server

---
+++ eureka-discovery-service/src/test/java/com/talissonmelo/eurekadiscoveryservice/EurekaDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talissonmelo.eurekadiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
