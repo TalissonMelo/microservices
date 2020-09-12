ebb3440aac648e0b525c097b8eb6e618ddc6eace
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-09-12 15:10:42 -0300

    User Service e Enable Eureka

--- microservice-user/src/main/java/com/talissonmelo/microserviceuser/MicroserviceUserApplication.java
+++ microservice-user/src/main/java/com/talissonmelo/microserviceuser/MicroserviceUserApplication.java
@@ -2,8 +2,10 @@ package com.talissonmelo.microserviceuser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroserviceUserApplication {
 
 	public static void main(String[] args) {
