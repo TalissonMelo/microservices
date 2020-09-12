6e8c57be067f412dbb7da4c08c28bc18471bbed4
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-09-12 12:40:25 -0300

    Projeto Microservices

---
+++ microservice-user/src/test/java/com/talissonmelo/microserviceuser/MicroserviceUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talissonmelo.microserviceuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
