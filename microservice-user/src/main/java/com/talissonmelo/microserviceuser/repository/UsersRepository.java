d805e52a679479dcdffec99de820775f77d9d15e
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-09-12 13:22:03 -0300

    Repositorio de Usuário

---
+++ microservice-user/src/main/java/com/talissonmelo/microserviceuser/repository/UsersRepository.java
@@ -0,0 +1,17 @@
+package com.talissonmelo.microserviceuser.repository;
+
+import com.talissonmelo.microserviceuser.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UsersRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUserUsername(String username);
+
+    @Query("select * from User u where u.id in (:ids)")
+    List<String> findByIdList(@Param("ids") List<Long> ids);
+}
