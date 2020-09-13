55a02ea11cfd4258e98d17599407260001c7579a
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-09-13 11:14:56 -0300

    Repositorio de Transação

--- microservice-course/bin/main/db/migration/V001__create_table_course.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE course (
-	id BIGINT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(255) NOT NULL,
-    author VARCHAR(255) NOT NULL,
-    category VARCHAR(255),
-    publish_date DATE,
-    
-    CONSTRAINT  PRIMARY KEY (id)
- )engine = InnoDB default charset=utf8;
