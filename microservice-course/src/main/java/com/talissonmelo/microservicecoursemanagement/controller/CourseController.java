89cce84ae4047f24991276273b348885b4fced39
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-09-15 18:30:53 -0300

    Criado Controlador de Curso

--- microservice-course/src/main/java/com/talissonmelo/microservicecoursemanagement/controller/CourseController.java
+++ microservice-course/src/main/java/com/talissonmelo/microservicecoursemanagement/controller/CourseController.java
@@ -1,9 +1,22 @@
 package com.talissonmelo.microservicecoursemanagement.controller;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.talissonmelo.microservicecoursemanagement.intercomm.UserClient;
+import com.talissonmelo.microservicecoursemanagement.model.Transaction;
+import com.talissonmelo.microservicecoursemanagement.service.CourseService;
 
 @RestController
 public class CourseController {
@@ -11,5 +24,34 @@ public class CourseController {
 	@Autowired
 	private UserClient userClient;
 	
+	@Autowired
+	private CourseService courseService;
 	
+	@GetMapping("/service/user/{userId}")
+    public ResponseEntity<?> findTransactionsOfUser(@PathVariable Long userId){
+        return ResponseEntity.ok(courseService.findTransactionsOfUser(userId));
+    }
+	
+	@GetMapping("/course/all")
+    public ResponseEntity<?> findAllCourses(){
+        return ResponseEntity.ok(courseService.findAll());
+    }
+	
+	@PostMapping("/course/enroll")
+    public ResponseEntity<?> saveTransaction(@RequestBody Transaction transaction) {
+        transaction.setDateOfIssue(LocalDateTime.now());
+        transaction.setCourse(courseService.findByCourseId(transaction.getCourse().getId()));
+        return new ResponseEntity<>(courseService.saveTransaction(transaction), HttpStatus.CREATED);
+    }
+	
+	@GetMapping("/service/course/{courseId}")
+    public ResponseEntity<?> findStudentsOfCourse(@PathVariable Long courseId){
+        List<Transaction> transactions = courseService.findTransactionsOfCourse(courseId);
+        if(CollectionUtils.isEmpty(transactions)){
+           return ResponseEntity.notFound().build();
+        }
+        List<Long> ids = transactions.parallelStream().map(t -> t.getUserId()).collect(Collectors.toList());
+        List<String> students = userClient.getUsername(ids);
+        return ResponseEntity.ok(students);
+    }
 }
