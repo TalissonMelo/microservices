73eb7afd591585ccf34d9f8af5a3dd2b2164cf01
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-09-19 11:42:01 -0300

    Serviço de gateway

---
+++ zuul-gateway-service/src/main/java/com/talissonmelo/zuulgatewayservice/ZuulGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.talissonmelo.zuulgatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayServiceApplication.class, args);
+	}
+
+}
