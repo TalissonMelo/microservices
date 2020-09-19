39a1f921ed7fc9c1bf3ffa4ecbff3b88ce0dbaf9
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-09-19 10:45:25 -0300

    Configurando CORS

--- microservice-course/src/main/java/com/talissonmelo/microservicecoursemanagement/MicroserviceCourseApplication.java
+++ microservice-course/src/main/java/com/talissonmelo/microservicecoursemanagement/MicroserviceCourseApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,6 +14,15 @@ public class MicroserviceCourseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceCourseApplication.class, args);
+	}
+	
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 
 }
